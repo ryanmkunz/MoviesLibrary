d7a12295891c2d9c2eefd1c390c67889dd9e9953
Ryan Kunz <ryanmkunz@gmail.com>
2019-05-07 15:29:37 -0500

    Initial commit
    Added api project with scaffolding

---
+++ MovieLibrary/Controllers/MoviesController.cs
@@ -0,0 +1,50 @@
+﻿using MovieLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MovieLibrary.Controllers
+{
+    public class MoviesController : ApiController
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+
+        // GET api/values
+        public IEnumerable<Movie> Get()
+        {
+            var movies = context.Movies.ToList();
+            return movies;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            var movie = context.Movies.FirstOrDefault(m => m.id == id).title.ToString();
+            return movie;
+        }
+
+        // POST api/values
+        public void Post([FromBody]Movie movie)
+        {
+            context.Movies.Add(movie);
+            context.SaveChanges();
+        }
+        
+        // PUT api/values/5
+        public void Put(int id, [FromBody]Movie movie)
+        {
+            if (context.Movies.Find(id) == null)
+            {
+                context.Movies.Add(movie);
+                context.SaveChanges();
+            }
+            else
+            {
+                
+            }
+        }
+    }
+}
---
+++ MovieLibrary/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MovieLibrary.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int id { get; set; }
+        public string title { get; set; }
+        public string genre { get; set; }
+        public string director { get; set; }
+    }
+}
