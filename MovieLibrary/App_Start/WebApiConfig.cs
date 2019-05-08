55fe60a6309e59204ef5a012b9cc35f6865507db
ryanmkunz <ryanmkunz@gmail.com>
2019-05-08 15:44:33 -0500

    Added search bar
    Using jQuery and AJAX to search for movies based on title

--- MovieLibrary/App_Start/WebApiConfig.cs
+++ MovieLibrary/App_Start/WebApiConfig.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Filters;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
 
@@ -11,7 +13,7 @@ namespace MovieLibrary
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
-        {
+        {            
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
--- MovieLibrary/Controllers/MoviesController.cs
+++ MovieLibrary/Controllers/MoviesController.cs
@@ -5,9 +5,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace MovieLibrary.Controllers
 {
+    [AllowCrossSite]
     public class MoviesController : ApiController
     {
         ApplicationDbContext context = new ApplicationDbContext();
